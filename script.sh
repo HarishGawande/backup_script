c396d028011a06c066b98a97ae44b593821fa87d
Harish_gawande <146774626+HarishGawande@users.noreply.github.com>
2024-02-25 00:36:21 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 #####################################The is Backup_script.sh
 
-echo -e "******** Hello! $USERNAME ********"
+echo -e " \n ******** Hello! $(whoami) ********"
 echo " ================================ "
 # to take in put from user
 echo -e " \n Get backup of New directory \n "
@@ -11,14 +11,14 @@ read -p "Enter the Destination_directory_name: " DDN
 
 
 # To create source_dir & backup_dir first
-mkdir /home/$USERNAME/Backup_Directory/$SDN
-mkdir /home/$USERNAME/Backup_Directory/$DDN
+sudo mkdir $(pwd)/$SDN
+sudo mkdir $(pwd)/$DDN
 
 # Source directory to be backed up
-source_dir="/home/$USERNAME/Backup_Directory/$SDN"
+source_dir="$(pwd)/$SDN"
 
 # Backup destination directory
-backup_dir="/home/$USERNAME/Backup_Directory/$DDN"
+backup_dir="$(pwd)/$DDN"
 
 # Timestamp for backup filename
 time_stamp=$(date +"%Y%m%d-%H%M%S")
@@ -30,10 +30,11 @@ backup_filepath="${backup_dir}/${backup_filename}"
 
 echo -e "Creating backup of $SDN... \n  "
 
-tar -czvf "$backup_filepath" "$source_dir"
+sudo tar -czvf "$backup_filepath" "$source_dir"
 echo " "
 if [ $? -eq 0 ]; then
   echo "Backup successful! Archive saved to: $backup_filepath"
 else
   echo "Backup failed. Check for errors."
 fi
+echo " "
